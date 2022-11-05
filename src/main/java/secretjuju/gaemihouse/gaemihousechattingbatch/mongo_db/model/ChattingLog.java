193f3a6ad7b247a7554aa1f5c5962655ca4a5fdf
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-11-06 00:31:09 +0900

    #3 MongoDB 연동 및 채팅 로그 조회

---
+++ src/main/java/secretjuju/gaemihouse/gaemihousechattingbatch/mongo_db/model/ChattingLog.java
@@ -0,0 +1,66 @@
+package secretjuju.gaemihouse.gaemihousechattingbatch.mongo_db.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "logs")
+public class ChattingLog {
+
+    @Id
+    private String id;
+    private String nickname;
+    private String message;
+    private String time;
+
+    public ChattingLog() {
+    }
+
+    public ChattingLog(String id, String nickname, String message, String time) {
+        this.id = id;
+        this.nickname = nickname;
+        this.message = message;
+        this.time = time;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "ChattingLog{" +
+                "id='" + id + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", message='" + message + '\'' +
+                ", time='" + time + '\'' +
+                '}';
+    }
+}
