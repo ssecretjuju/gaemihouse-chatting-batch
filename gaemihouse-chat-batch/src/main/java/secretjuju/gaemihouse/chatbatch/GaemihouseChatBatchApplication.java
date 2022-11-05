a6a2e1a26fa7222f3df9fb8bccd7467283d33252
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-11-05 17:05:15 +0900

    #1 배치 서버 구축에 필요한 의존성 설정

---
+++ gaemihouse-chat-batch/src/main/java/secretjuju/gaemihouse/chatbatch/GaemihouseChatBatchApplication.java
@@ -0,0 +1,13 @@
+package secretjuju.gaemihouse.chatbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaemihouseChatBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaemihouseChatBatchApplication.class, args);
+    }
+
+}
---
+++ gaemihouse-chat-batch/src/test/java/secretjuju/gaemihouse/chatbatch/GaemihouseChatBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package secretjuju.gaemihouse.chatbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaemihouseChatBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
